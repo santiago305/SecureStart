25696ac6f821d048afd366a06f2cb8f2140613cb
santiago305 <minecratf633@gmail.com>
2025-10-21 12:43:09 -0500

    Routes corregidos

--- src/main.tsx
+++ src/main.tsx
@@ -25,7 +25,7 @@ import { StrictMode, Suspense } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import { RouterProvider } from 'react-router-dom';
-import { router } from './router/Router.tsx';
+import { router } from './Router/Router.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
